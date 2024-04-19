d4d98c200e317fe36758014eccc4f1f0488b5809
Koyonari <yongshyan.an@gmail.com>
2024-04-19 13:54:20 +0800

    Update comments

--- week01/practical2/app.js
+++ week01/practical2/app.js
@@ -1,28 +1,29 @@
+//Open port
 const express = require('express');
 const app = express();
 const port = 3000;
 
-//Hello World! on homepage
+//Part 1: Hello World! on homepage
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-//Respond to POST req
+////Part 2: Respond to POST req
 app.post('/', (req, res) => {
     res.send('Got a POST request!')
 });
 
-//Respond to PUT req
+//Part 3: Respond to PUT req
 app.put('/user', (req, res) => {
     res.send('Got a POST request at /user!')
 });
 
-//Respond to DELETE req
+//Part 4: Respond to DELETE req
 app.delete('/user', (req, res) => {
     res.send('Got a DELETE request at /user!')
 });
 
-//Serving static files
+//Part 5: Serving static files
 app.use(express.static('public'));
 
 //Print out in terminal that port is open
