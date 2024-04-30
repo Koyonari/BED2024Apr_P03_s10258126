4eed45f6caa08e979cfac621bd7d76421b2615c2
Koyonari <yongshyan.an@gmail.com>
2024-04-30 23:38:17 +0800

    Add practical 3

---
+++ week03/books-api-mvc/app.js
@@ -0,0 +1,23 @@
+//Import modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const booksController = require("./controllers/booksController"); // Import controllers
+const validateBook = require("./middlewares/validateBook");
+
+//Initialise Express and configure Middleware
+const app = express();
+app.use(bodyParser.json()); // Parse incoming JSON data in request body
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+//Define individual routes for each controller function
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", validateBook, booksController.createBook); //Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); //Add validateBook before updateBook
+app.delete("/books/:id", booksController.deleteBook);
+
+//Define port and start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
